58d1fbd433dc398498d16871f6f479b1bb0cb87a
rddomino <seltos@bk.ru>
2021-03-09 21:34:18 +0600

    Добавлен JS, модальное окно, скрипт появления и его закрытия

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let close = document.querySelector('#close');
+
+button.addEventListener('click', function() {
+
+    modal.classList.add('modal_active');
+
+});
+
+close.addEventListener('click', function() {
+
+    modal.classList.remove('modal_active');
+
+});
