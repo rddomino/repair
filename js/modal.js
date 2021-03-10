8c6dbab58007bdd72b3faa12b27808519277d393
rddomino <seltos@bk.ru>
2021-03-10 21:26:05 +0600

    Добавлена библиотека jQuery

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    let button = $('#button');
+    let modal = $('#modal');
+    let close = $('#close');
+    let offer__button = $('offer__button');
+
+    button.on('click', function(){
+        modal.addClass('modal_active');
+    });
+
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
+
+    offer__button.on('click', function(){
+        $('body').scrollTop(0);
+    });
+
+})
