1a6a805be3ceb93f52a5c666fca094eb0c2145f7
rddomino <seltos@bk.ru>
2021-03-19 20:46:59 +0600

    Добавлен обработчик формы AJAX

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,27 @@ function defaultTask(cb) {
   
   exports.default = defaultTask
   
+
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
  
 gulp.task('minify-css', () => {
   return gulp.src('css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('htmlmin', () => {
+  return gulp.src('*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+ 
+const minify = require('gulp-minify');
+ 
+gulp.task('compress', function() {
+  gulp.src(['js/*.js', 'js/*.mjs'])
+    .pipe(minify())
+    .pipe(gulp.dest('dist/js'))
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,71 @@
-let button = document.querySelector('#button');
-let modal = document.querySelector('#modal');
-let close = document.querySelector('#close');
+// Инициализация библиотеки WOW
+   
+        new WOW().init();
 
-button.addEventListener('click', function() {
+        $(document).ready(function(){
 
-    modal.classList.add('modal_active');
+            /* Валидация формы */
+            $('#brif-form').validate({
+                rules: {
+                        // simple rule, converted to {required:true}
+                        username: {
+                            required: true,
+                            minlength: 2
+                        },
+                        // compound rule
+                        email: {
+                            required: true,
+                            email: true
+                        }
+                },
+                messages: {
+                    username: {
+                        required: "Укажите ваше имя",
+                        minlength: jQuery.validator.format("Имя должно быть не меньше {0} символов!")
+                    },
+                    email: "Укажите вашу почту"
+                },
+                errorClass: "invalid"
+            });
+            /* Валидация формы */
+            $('#offer-form').validate({
+                rules: {
+                        // simple rule, converted to {required:true}
+                        username: {
+                            required: true,
+                            minlength: 2
+                        },
+                        // compound rule
+                        email: {
+                            required: true,
+                            email: true
+                        }
+                },
+                messages: {
+                    username: {
+                        required: "Укажите ваше имя",
+                        minlength: jQuery.validator.format("Имя должно быть не меньше {0} символов!")
+                    },
+                    email: "Укажите вашу почту"
+                },
+                errorClass: "invalid"
+            });
 
-});
+            /* Валидация плагина маска телефона */
+            $('#phone').mask('+7 (999) 999-99-99');
 
-close.addEventListener('click', function() {
+            // Обработка и отправка формы через AJAX
+            $('#offer-form').on('submit', function(event){
+                event.preventDefault();
+                $.ajax({
+                    url: 'mail.php',
+                    type: 'POST',
+                    data: $(this).serialize(),
+                    success: function(data){
+                        console.log(data);
+                        $('.success').text(data + ', Ваша форма отправлена.');
+                    }
+                });
+            });
 
-    modal.classList.remove('modal_active');
-
-});
+        }); //закрытие
